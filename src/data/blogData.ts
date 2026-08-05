9d04e705a568a3da417ee6bbf27d6b4efd7f4859
randomchaffee <107630700+randomchaffee@users.noreply.github.com>
2026-08-05 09:07:52 +0800

    test: check if workflow runs

--- src/data/blogData.ts
+++ src/data/blogData.ts
@@ -3,15 +3,21 @@ import type { PostItem } from "../types/portfolio";
 
 export const BLOG_POSTS: PostItem[] = [
    {
-      id: 1,
-      slug: "why-i-started-this",
-      title: "Why I Started Creating This Website",
-      tags: ["Self Reflection", "Web Development"],
-      excerpt: "idk i just felt like it",
-      content: "idk i just felt like it",
-      publishedAt: "August 02, 2026"
-   },
-   {
+      id: 3,
+      slug: "first-day-junior-year",
+      title: "First Day This Semester And My First Lecture Was Cancelled Unannounced",
+      tags: ["University", "School"],
+      excerpt: `Today I woke up early to prepare for my first day in class (it's a single lecture), 
+               only to find out that the lecture was cancelled abruptly due being "sacrificed to...`,
+      content: `Today I woke up early to prepare for my first day in class (it's a single lecture), 
+               only to find out that the lecture was cancelled abruptly due being "sacrificed to the academic gods" and 
+               losing the scheduling battle. I wish they could've just announced it through email or class announcement,
+               but hey, there's another problem! There isn't any online classroom or group even set up yet.
+                              
+               oh well, back to sleep ig.`,
+      publishedAt: "August 03, 2026"
+   },  
+         {
       id: 2,
       slug: "about-my-previous-portfolio",
       title: "Some Concerns About My Previous Portfolio",
@@ -32,18 +38,12 @@ export const BLOG_POSTS: PostItem[] = [
       publishedAt: "August 02, 2026"
    },
    {
-      id: 3,
-      slug: "first-day-junior-year",
-      title: "First Day This Semester And My First Lecture Was Cancelled Unannounced",
-      tags: ["University", "School"],
-      excerpt: `Today I woke up early to prepare for my first day in class (it's a single lecture), 
-               only to find out that the lecture was cancelled abruptly due being "sacrificed to...`,
-      content: `Today I woke up early to prepare for my first day in class (it's a single lecture), 
-               only to find out that the lecture was cancelled abruptly due being "sacrificed to the academic gods" and 
-               losing the scheduling battle. I wish they could've just announced it through email or class announcement,
-               but hey, there's another problem! There isn't any online classroom or group even set up yet.
-                              
-               oh well, back to sleep ig.`,
-      publishedAt: "August 03, 2026"
-   },  
+      id: 1,
+      slug: "why-i-started-this",
+      title: "Why I Started Creating This Website",
+      tags: ["Self Reflection", "Web Development"],
+      excerpt: "idk i just felt like it",
+      content: "idk i just felt like it",
+      publishedAt: "August 02, 2026"
+   }
 ]
